dc18228c48d0bbd714521b75c681c6c85c94ac47
Ninotro <ninotroia96@gmail.com>
2023-11-23 12:28:18 +0100

    Initial commit

---
+++ src/main/java/com/experis/course/spring/repository/PhotoRepository.java
@@ -0,0 +1,11 @@
+package com.experis.course.spring.repository;
+
+import com.experis.course.spring.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository <Photo, Integer> {
+
+    List <Photo> findByTitleContainingIgnoreCase(String nameKeyword);
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO photos (title, description, url, visible) VALUES ('Mountain Landscape', 'Majestic mountain view', 'https://example.com/mountain.jpg', true);
+
+INSERT INTO photos (title, description, url, visible) VALUES ('Ocean Sunset', 'Beautiful sunset over the ocean', 'https://example.com/sunset.jpg', true);
+
+INSERT INTO photos (title, description, url, visible) VALUES('City Skyline', 'Cityscape at night', 'https://example.com/cityscape.jpg', true);
+
+INSERT INTO photos (title, description, url, visible) VALUES('Flower Garden', 'Colorful flowers in a garden', 'https://example.com/flowers.jpg', true);
+
+INSERT INTO photos (title, description, url, visible) VALUES('Snowy Forest', 'Scenic view of a snowy forest', 'https://example.com/snowy-forest.jpg', true);
